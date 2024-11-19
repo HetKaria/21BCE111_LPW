f1018b416360f7ce4ea99e944f89c61eafea99c9
21bce111 <21bce111@nirmauni.ac.in>
2024-11-19 12:01:31 +0530

    21BCE111_LPW

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import redirect, render_template, Response, Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/hospital', methods=['POST', 'GET'])
+def add_hospital():
+    return render_template("Hospital.html")
+
+@app.route('/patients', methods= ["POST", "GET"])
+def add_Patients():
+    return render_template("Patients.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
